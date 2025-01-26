4696eff31459ea573c00c92de81e0f513f7dae72
Luisma <76691533+Luisma535@users.noreply.github.com>
2025-01-26 01:36:38 -0500

    Update db-help.sql

--- db/db-help.sql
+++ db/db-help.sql
@@ -2,9 +2,11 @@ CREATE DATABASE videos
  
 CREATE TABLE peliculas( 
     name VARCHAR(50) NOT NULL,
-    path VARCHAR(200) NOT NULL 
+    path VARCHAR(200) NOT NULL,
+    id Int(50) NOT NULL
 )
 CREATE TABLE music( 
     name VARCHAR(50) NOT NULL,
-    path VARCHAR(200) NOT NULL
+    path VARCHAR(200) NOT NULL,
+    id Int(50) NOT NULL
 )
